618ef91476e2048384330de28e1cec9f785c1b95
sergeybv26 <sergey.bv26@gmail.com>
2022-03-25 16:19:18 +0300

    Добавлены шаблоны

--- views.py
+++ views.py
@@ -1,10 +1,59 @@
 """Описание представлений"""
 from framework.templator import render
 
+# Так как работаем без базы данных, создадим список с параметрами товаров
+PRODUCTS = [
+    {
+        'name': '',
+        'price': 1000,
+        'desc': '',
+        'img': '/static/img/1.jpg'
+    },
+{
+        'name': '',
+        'price': 1000,
+        'desc': '',
+        'img': '/static/img/1.jpg'
+    },
+{
+        'name': '',
+        'price': 1000,
+        'desc': '',
+        'img': '/static/img/1.jpg'
+    },
+{
+        'name': '',
+        'price': 1000,
+        'desc': '',
+        'img': '/static/img/1.jpg'
+    },
+{
+        'name': '',
+        'price': 1000,
+        'desc': '',
+        'img': '/static/img/1.jpg'
+    },
+]
+
+
+def get_main_product():
+    """
+    Получает горячие товары
+    :return: Список словарей
+    """
+    return PRODUCTS[-3:]
+
 
 class Index:
     """Представление главной страницы"""
+
     def __call__(self, request):
         title = 'Главная'
 
-        return '200 OK', render('index.html', title=title)
+        context = {
+            'title': title,
+            'year': request.get('year'),
+            'product_list': get_main_product()
+        }
+
+        return '200 OK', render('index.html', context=context)
