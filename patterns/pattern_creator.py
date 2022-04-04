c5e9681ae40faa8b938570902ce843346e6f372c
sergeybv26 <sergey.bv26@gmail.com>
2022-04-04 16:56:46 +0300

    необходимо протестировать и добавить логгирование

--- patterns/pattern_creator.py
+++ patterns/pattern_creator.py
@@ -1,3 +1,6 @@
+from quopri import decodestring
+
+
 class AbstractUser:
     """Класс абстрактного пользователя"""
     def __init__(self, name, password):
@@ -88,22 +91,144 @@ class Engine:
 
     @staticmethod
     def create_user(user_type, name, password):
+        """
+        Создает пользователя
+        :param user_type: тип пользователя (покупатель или сотрудник)
+        :param name: имя пользователя
+        :param password: пароль
+        :return: экземпляр класса пользователя соответствующего типа
+        """
         return UserFactory.create(user_type, name, password)
 
     @staticmethod
     def create_category(name, category=None):
+        """
+        Создает категорию
+        :param name: имя категории
+        :param category: имя родительской категории, если создаем подкатегорию
+        :return: экземпляр класса категории
+        """
         return Category(name, category)
 
     def get_category_by_id(self, _id):
+        """
+        Осуществляет поиск категории по id
+        :param _id: id искомой категории
+        :return: экземпляр класса категории
+        """
         for category in self.categories:
             if category.id == _id:
                 return category
         raise Exception(f'Не найдена категория с id = {_id}')
 
     def get_category_by_name(self, name):
+        """
+        Осуществляет поиск категории по имени
+        :param name: имя категории
+        :return: экземпляр класса категории
+        """
         for category in self.categories:
             if category.name == name:
                 return category
         raise Exception(f'Категория {name} не найдена')
 
+    def get_all_categories(self):
+        """Получает список всех категорий"""
+        return self.categories
 
+    @staticmethod
+    def create_product(product_type, name, category, price):
+        """
+        Создает товар
+        :param product_type: Тип товара (продукт или услуга)
+        :param name: наименование товара
+        :param category: категория товара
+        :param price: цена товара
+        :return: экземпляр соответствующего класса товара
+        """
+        return ProductFactory.create(product_type, name, category, price)
+
+    def get_product_by_id(self, _id):
+        """
+        Выполняет поиск товара по id
+        :param _id: id товара
+        :return: экземпляр класса товара
+        """
+        for product in self.products:
+            if product.id == _id:
+                return product
+        raise Exception(f'Не найден продукт с id = {_id}')
+
+    def get_product_by_name(self, name):
+        """
+        Выполняет поиск товара по имени
+        :param name: имя товара
+        :return: экземпляр класса товара
+        """
+        for product in self.products:
+            if product.name == name:
+                return product
+        raise Exception(f'Продукт {name} не найден')
+
+    def get_all_products(self):
+        """Получает список всех товаров"""
+        return self.products
+
+    def get_main_products(self):
+        """Получает список популярных товаров"""
+        main_products = []
+        try:
+            main_products = self.products[-3:]
+        except IndexError:
+            print('Отсутствуют товары в базе')
+        return main_products
+
+    def get_products_by_category(self, category_id):
+        """Получает список продуктов категории"""
+        for category in self.categories:
+            if category.id == category_id:
+                return category.products
+        return []
+
+    @staticmethod
+    def decode_value(val):
+        """
+        Осуществляет декодирование кириллицы и специальных символов
+        :param val: декодируемое значение
+        :return: декодированное значение
+        """
+        value_bytes = bytes(val.replace('%', '=').replace('+', ' '), 'UTF-8')
+        value_decode_str = decodestring(value_bytes).decode('UTF-8')
+
+        return value_decode_str
+
+
+class SingletonByName(type):
+    """Метакласс синглтон по имени"""
+    def __init__(cls, name, bases, attrs, **kwargs):
+        super().__init__(name, bases, attrs)
+        cls.__instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if args:
+            name = args[0]
+        elif kwargs:
+            name = kwargs['name']
+        else:
+            name = None
+
+        if name in cls.__instance:
+            return cls.__instance[name]
+        else:
+            cls.__instance[name] = super().__call__(*args, **kwargs)
+            return cls.__instance[name]
+
+
+class Logger(metaclass=SingletonByName):
+    """Класс логгера"""
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def log(text):
+        print('log--->', text)
--- views.py
+++ views.py
@@ -1,53 +1,9 @@
 """Описание представлений"""
 from framework.templator import render
+from patterns.pattern_creator import Engine, Logger
 
-# Так как работаем без базы данных, создадим список с параметрами товаров
-PRODUCTS = [
-    {
-        'name': 'Полка для обуви, металл, дерево, 3 секции, 76х31х49 см, с сиденьем, орех, бежевая, Sheffilton',
-        'category': 'Хранение',
-        'price': 4726,
-        'desc': 'Полка-банкетка является отличным решением для домашнего интерьера',
-        'img': '/static/img/1.jpeg'
-    },
-    {
-        'name': 'Вешалка напольная Sheffilton M-3026 В1-60, венге/черный',
-        'category': 'Хранение',
-        'price': 6958,
-        'desc': 'Функциональная компакт-прихожая',
-        'img': '/static/img/2.jpg'
-    },
-    {
-        'name': 'Качели садовые Кокон, 150 кг, Green Days, серые, ротанг, подушка малиновая',
-        'category': 'Сад',
-        'price': 26991,
-        'desc': 'Подвесное кресло Кокон Green Days идет в комплекте со стойкой',
-        'img': '/static/img/3.jpg'
-    },
-    {
-        'name': 'Постельное белье евростандарт, простыня 220х240 см, 2 наволочки 50х70 см, пододеяльник 200х220 см',
-        'category': 'Текстиль',
-        'price': 3509,
-        'desc': 'Постельное белье из поплина обладает хорошей воздухопроницаемостью и при этом прекрасно держит тепло. '
-                'Белье мягкое, легко гладится и обладает прекрасными органолептическими свойствами',
-        'img': '/static/img/4.jpg'
-    },
-    {
-        'name': 'Плед 1.5-спальный, 150х180 см',
-        'category': 'Текстиль',
-        'price': 2290,
-        'desc': 'Плед произведён из натурального хлопка и бамбука',
-        'img': '/static/img/5.jpg'
-    },
-]
-
-
-def get_main_product():
-    """
-    Получает популярные товары
-    :return: Список словарей
-    """
-    return PRODUCTS[-3:]
+ENGINE = Engine()
+LOGGER = Logger('main')
 
 
 class Index:
@@ -60,7 +16,7 @@ class Index:
             'title': title,
             'year': request.get('year'),
             'path': request.get('path'),
-            'product_list': get_main_product()
+            'product_list': ENGINE.get_main_products()
         }
 
         return '200 OK', render('index.html', context=context)
@@ -68,6 +24,7 @@ class Index:
 
 class Products:
     """Представление страницы с продуктами"""
+
     def __call__(self, request):
         title = 'Продукты'
 
@@ -75,7 +32,98 @@ class Products:
             'title': title,
             'year': request.get('year'),
             'path': request.get('path'),
-            'product_list': PRODUCTS
+            'product_list': ENGINE.get_all_products(),
+            'category_list': ENGINE.get_all_categories()
+        }
+
+        return '200 OK', render('products.html', context=context)
+
+
+class CreateCategory:
+    """Представление страницы создания категории"""
+
+    def __call__(self, request):
+        title = 'Создание категории'
+
+        context = {
+            'title': title,
+            'year': request.get('year'),
+            'path': request.get('path'),
+            'product_list': ENGINE.get_all_products(),
+            'category_list': ENGINE.get_all_categories()
+        }
+
+        if request['method'] == 'POST':
+            data = request['data']
+            name = data['name']
+            name = ENGINE.decode_value(name)
+            category_id = data.get('category_id')
+            category = None
+
+            if category_id.isdigit():
+                category = ENGINE.get_category_by_id(int(category_id))
+
+            new_category = ENGINE.create_category(name, category)
+
+            ENGINE.categories.append(new_category)
+            return '302 Found', [('Location', '/')]
+        else:
+            return '200 OK', render('create_category.html', context=context)
+
+
+class CreateProduct:
+    """Представление страницы создания продукта"""
+    category_id = - 1
+
+    def __call__(self, request):
+        title = 'Создание категории'
+
+        context = {
+            'title': title,
+            'year': request.get('year'),
+            'path': request.get('path'),
+            'category_list': ENGINE.get_all_categories(),
+            'error': ''
+        }
+
+        if request['method'] == 'POST':
+            data = request['data']
+            try:
+                category_id = data['category_id']
+            except KeyError:
+                context['error'] = 'Необходимо выбрать категорию'
+                return '200 OK', render('create_product.html', context=context)
+            product_type = data['product_type']
+            name = data['name']
+            price = data['price']
+
+            category = ENGINE.get_category_by_id(int(category_id))
+            product = ENGINE.create_product(product_type, name, category, price)
+            ENGINE.products.append(product)
+            return '302 Found', [('Location', f'/products/category/?id={category_id}')]
+        else:
+            return '200 OK', render('create_product.html', context=context)
+
+
+class ProductsList:
+    """Представление страницы товаров для категории"""
+    category_id = -1
+
+    def __call__(self, request):
+        self.category_id = request['request_params']['id']
+        if self.category_id != -1:
+            products_list = ENGINE.get_products_by_category(int(self.category_id))
+        else:
+            products_list = []
+
+        title = f'Продукты'
+
+        context = {
+            'title': title,
+            'year': request.get('year'),
+            'path': request.get('path'),
+            'product_list': products_list,
+            'category_list': ENGINE.get_all_categories()
         }
 
         return '200 OK', render('products.html', context=context)
@@ -83,6 +131,7 @@ class Products:
 
 class Contacts:
     """Представление страницы контактов"""
+
     def __call__(self, request):
         title = 'Контакты'
 
